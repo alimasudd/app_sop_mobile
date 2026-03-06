14d084bc94658c36d5021cf97176aa068f433402
alimasudd <alimasudd.dev@gmail.com>
2026-03-06 10:52:02 +0700

    update relative imports to absolute imports in auth views

--- lib/app/modules/auth/views/login_view.dart
+++ lib/app/modules/auth/views/login_view.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import '../controllers/auth_controller.dart';
-import '../../../routes/app_pages.dart';
+import 'package:test_maret/app/routes/app_pages.dart';
 
 class LoginView extends GetView<AuthController> {
   const LoginView({Key? key}) : super(key: key);
--- lib/app/modules/auth/views/register_view.dart
+++ lib/app/modules/auth/views/register_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import '../controllers/auth_controller.dart';
+import 'package:test_maret/app/modules/auth/controllers/auth_controller.dart';
 
 class RegisterView extends GetView<AuthController> {
   const RegisterView({Key? key}) : super(key: key);
